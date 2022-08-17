e7df7ff801e39252d06382e0332bcfd5bed6dfc3
Hannes Kühnel <hanneskuehnel@gmx.de>
2022-08-17 16:15:13 +0200

    Initial Commit

---
+++ BonprixCodeChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BonprixCodeChallenge
+//
+//  Created by Hannes Kühnel on 17.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
